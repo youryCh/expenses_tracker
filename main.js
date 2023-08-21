150a88773fa1905ffd6b48c10a75125315b4e678
Xolmes <xolmes@ya.ru>
2023-08-21 22:40:37 +0300

    add app icon

--- main.js
+++ main.js
@@ -8,6 +8,7 @@ const createWindow = () => {
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
     },
+    icon: path.join(__dirname, '/asset/main.ico')
   });
 
   win.loadFile('index.html');
